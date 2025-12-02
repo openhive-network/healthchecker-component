bab17e4a0e1f2b201f3a080df6e34a744ddd1d75
jlachor <jlachor@syncad.com>
2025-12-02 14:35:42 +0100

    Fix text error in confirmation dialog

--- src/components/healthchecker/ConfirmationSwitchDialog.tsx
+++ src/components/healthchecker/ConfirmationSwitchDialog.tsx
@@ -32,7 +32,7 @@ const ConfirmationSwitchDialog: React.FC<ConfirmationSwitchDialogProps> = ({
           <span className="font-semibold">{providerLink}</span>?
         </div>
         <DialogFooter>
-          <Button onClick={() => onDialogOpenChange(false)} variant="outline">
+          <Button onClick={() => onDialogOpenChange(false)} variant="outline" className="text-white dark:text-black">
             Cancel
           </Button>
           <Button onClick={onConfirm}>Confirm</Button>
